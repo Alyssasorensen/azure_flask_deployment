a65547d06953f5b99262bcd38cb3ee0d8a886639
Alyssasorensen <-m>
2023-09-13 15:04:22 +0000

    updated the app.py file

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# Load data from a CSV file (make sure your CSV file is in the project directory)
+data = pd.read_csv('aggregate-household-income-in-the-past-12-months-in-2015-inflation-adjusted-dollars.csv')  
 @app.route('/')
-def index():
-    return render_template('base.html')
+def home():
+    return render_template('data.html', data=data)
 
 if __name__ == '__main__':
-    app.run(
-        debug=True,
-        port=8080
-    )
+    app.run(debug=True)
