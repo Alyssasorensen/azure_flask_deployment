44c8423481f6a42b88387439435222132645be31
Alyssasorensen <-m>
2023-09-13 15:22:47 +0000

    updated the app.py file

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load data from a CSV file (make sure your CSV file is in the project directory)
-data = pd.read_csv('aggregate-household-income-in-the-past-12-months-in-2015-inflation-adjusted-dollars.csv')  
 @app.route('/')
-def home():
+def index():
+    return render_template('base.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+df = pd.read_csv('https://raw.githubusercontent.com/Alyssasorensen/azure_flask_deployment/main/datasets/aggregate-household-income-in-the-past-12-months-in-2015-inflation-adjusted-dollars.csv')
+@app.route('/data')
+def data(data=df):
+    data = data.sample(15)
     return render_template('data.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        port=8080
+    )
